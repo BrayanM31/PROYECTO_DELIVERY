df54c84ed37ac5e7c3597dcde360ea2a4f1be7f2
BrayanM31 <brayan.rodriguez@epn.edu.ec>
2024-07-11 09:58:29 -0500

    ModMain Main 1.0.1.1

--- PROYECTO_ENCOMIENDA/src/proyecto_encomienda/PROYECTO_ENCOMIENDA.java
+++ PROYECTO_ENCOMIENDA/src/proyecto_encomienda/PROYECTO_ENCOMIENDA.java
@@ -14,7 +14,7 @@ public class PROYECTO_ENCOMIENDA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola soy");
+        System.out.println("Hola ");
         // TODO code application logic here
     }
     
